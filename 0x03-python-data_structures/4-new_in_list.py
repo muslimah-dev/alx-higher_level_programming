7d144370d6981e71bea3b1e187a88d441c3bcb34
Oluwadamilola <suncodesy@gmail.com>
2022-06-06 12:22:23 +0000

    create 4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        return (my_list)
+    if idx > len(my_list) - 1:
+        return (my_list)
+    n_l = my_list[:]
+    return (n_l)
