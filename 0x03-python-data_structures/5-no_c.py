62efb64969936024d36e5537b1fa33501a4bbb45
Oluwadamilola <suncodesy@gmail.com>
2022-06-06 13:39:09 +0000

    update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,5 +5,5 @@ def no_c(my_string):
     for i in range(len(my_string)):
         if (my_string[i] == 'c' or my_string[i] == 'C'):
             new_s = new_s[:(i - l)] + my_string[(i + 1):]
-            j += 1
+            l += 1
     return (new_s)
