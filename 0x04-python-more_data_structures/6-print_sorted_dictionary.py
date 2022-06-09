a772a07700126f74517ef165f1574a5d67d9108b
Oluwadamilola <suncodesy@gmail.com>
2022-06-09 15:08:41 +0000

    create 6-print_sorted_dictionary.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     new_order = list(a_dictionary.keys())
-    new_order.sort()
     for i in new_order:
         print("{}: {}".format(i, new_order.get(i)))
