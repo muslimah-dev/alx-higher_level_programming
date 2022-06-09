4134c461610006608b6e0a56dfe90b88fbb6b87d
Oluwadamilola <suncodesy@gmail.com>
2022-06-09 17:11:01 +0000

    update 7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
-    for x, y in a_dictionary.items():
-        print(x, ': ', y)
+    return(a_dictionary)
