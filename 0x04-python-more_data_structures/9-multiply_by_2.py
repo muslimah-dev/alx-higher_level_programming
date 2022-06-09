b30de128bd29172bf7ac8981812a7031f705e4a3
Oluwadamilola <suncodesy@gmail.com>
2022-06-09 22:31:18 +0000

    create 9-multiply_by_2.py

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/bin/usr/python3
+def multiply_by_2(a_dictionary):
+    new_dict = a_dictionary.copy
+    dict_list = list(new_dict.keys())
+    for i in dict_list:
+        new_dict[i] *= 2
+    return (new_dict)
