e400f625a1ea74dea0ea53a30f42104b15bd0607
Oluwadamilola <suncodesy@gmail.com>
2022-06-12 20:39:58 +0000

    create 9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    my_list_length = len(my_list)
+    if my_list_length == 0:
+        return (None)
+    max_ = my_list[0]
+    for i in range(1, my_list_length):
+        if my_list[i] == max_:
+            max_ = my_list[i]
+    return (max_)
