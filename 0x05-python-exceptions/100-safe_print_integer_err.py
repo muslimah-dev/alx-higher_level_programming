ff91ae6a2c7ee7d5756baefe83bd4b3805e4009c
Oluwadamilola <suncodesy@gmail.com>
2022-06-20 21:42:17 +0000

    lala

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    """prints an integer"""
+    try:
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError) as err:
+        print("Exception:", err, file=sys.stderr)
+        return False
