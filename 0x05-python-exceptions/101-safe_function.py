4d87a67b6f58293dc4c40af61a3a228fcc17e899
Oluwadamilola <suncodesy@gmail.com>
2022-06-20 21:46:13 +0000

    create 101-safe_function.py

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    """Executes a function safely."""
+    try:
+        result = fct(*args)
+    except Exception as err:
+        result = None
+        print("Exception:", err, file=sys.stderr)
+    return result
