e51d3059d878bf83efb7035283232951e0b7746f
Oluwadamilola <suncodesy@gmail.com>
2022-06-20 20:40:04 +0000

    create 1-safe_print_integer.py

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    """prints an integer with "{:d}".format()"""
+    try:
+        print("{:d}".format(value))
+    except (TypeError, ValueError):
+        return False
+    else:
+        return True
