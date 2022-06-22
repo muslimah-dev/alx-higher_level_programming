b283c4a22f1fea204e89b8d97340398b1e860637
Oluwadamilola <suncodesy@gmail.com>
2022-06-22 09:25:00 +0000

    create 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" class Square that defines a square by Private instance attribute: size."""
+
+
+class Square:
+    """class Square with a private variable "size"."""
+    
+
+    def __init__(self, size):
+        """Initializes a square.
+
+
+        Args:
+            size(int): size of Square
+            """
+        self.__size = size
