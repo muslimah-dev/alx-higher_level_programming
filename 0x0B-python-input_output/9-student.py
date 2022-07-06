1508f49f752a76231174458a0753a9f823e85517
Oluwadamilola <suncodesy@gmail.com>
2022-07-06 09:57:47 +0000

    create 9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""changes student class to JSON"""
+
+class Student:
+    """ student class with attributes 
+    first_name, last_name and age
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """changes class to json"""
+        return self.__dict__.copy()
