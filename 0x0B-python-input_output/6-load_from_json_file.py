a01283c4594f16f9abb4a615160c2b7600032009
Oluwadamilola <suncodesy@gmail.com>
2022-07-06 05:45:26 +0000

    create 6-load_from_json_file.py

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/puthon3
+"""function that creates an object frim a JSON file."""
+
+
+def load_from_json_file(filename):
+    """Creates an object
+    Args:
+        filenane: name of the file
+    Returns: the fileobject created.
+    """
+    with open(filename, 'r') as f:
+        return json.load(f)
